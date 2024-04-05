27a299912eaa8f946087847bef52bfbc1522a079
Ydesaulty <yrae9@hotmail.fr>
2024-04-05 15:44:48 +0200

    Installation_Swagger

--- Projet_Studi_GoldenLine/Projet_Studi_GoldenLine/settings.py
+++ Projet_Studi_GoldenLine/Projet_Studi_GoldenLine/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-hb&)1m7*(=ehg*sdsxx)ypmbzp_%z@n+t%id(ly@@sdj*p&kq#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.pythonanywhere.com']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'indicateur.apps.IndicateurConfig',
+    'drf_yasg'
 ]
 
 REST_FRAMEWORK = {
--- Projet_Studi_GoldenLine/Projet_Studi_GoldenLine/urls.py
+++ Projet_Studi_GoldenLine/Projet_Studi_GoldenLine/urls.py
@@ -18,14 +18,30 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import index
-from django.urls.conf import include
-from rest_framework import routers
+from django.conf.urls import include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import routers, permissions
 from indicateur.urls import router as indicateur_routeur
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Collecte API",
+      default_version='v1',
+      description="API utilisee pour générer un indicateur de panier moyen",
+      contact=openapi.Contact(email="yrae9@hotmail.com"),      
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.registry.extend(indicateur_routeur.registry)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include(router.urls))
+   path('admin/', admin.site.urls),
+   path('', include(router.urls)),
+   path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),   
 ]
