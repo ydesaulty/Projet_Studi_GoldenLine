755c80205b835d909acf8a81e479ca256c149a86
Ydesaulty <yrae9@hotmail.fr>
2024-08-03 13:25:13 +0200

    Views_queryset

--- Projet_Studi_GoldenLine/Projet_Studi_GoldenLine/settings.py
+++ Projet_Studi_GoldenLine/Projet_Studi_GoldenLine/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'indicateur.apps.IndicateurConfig',
-    'drf_yasg'
+    'drf_yasg',
+    'corsheaders'
 ]
 
 REST_FRAMEWORK = {
@@ -58,6 +59,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'Projet_Studi_GoldenLine.urls'
--- Projet_Studi_GoldenLine/indicateur/views.py
+++ Projet_Studi_GoldenLine/indicateur/views.py
@@ -7,39 +7,56 @@ from indicateur.models import Collecte, Article, Csp, Client
 
 from indicateur.serializers import CollecteSerializer, ArticleSerializer, CspSerializer, CombinedSerializer
 
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
 class CollecteViewSet(viewsets.ModelViewSet):
-    
     serializer_class = CollecteSerializer
 
     def get_queryset(self):
-        queryset = Collecte.objects.filter()
+        queryset = Collecte.objects.all()
         id_client = self.request.GET.get('id_client')
-        if id_client :
+        if id_client:
             queryset = queryset.filter(id_client=id_client)
         return queryset
-         
 
 class ArticleViewSet(viewsets.ModelViewSet):
-    
     serializer_class = ArticleSerializer
-    
-    queryset = Article.objects.all()    
+    queryset = Article.objects.all()
     filterset_fields = ['categorie_achat', 'description']
     search_fields = ['description']
 
 class CspViewSet(viewsets.ModelViewSet):
-
     serializer_class = CspSerializer
-    
-    def get_queryset(self):        
-        return Csp.objects.filter()
-
-class CombinedModelViewSet(viewsets.ModelViewSet):
-    
-    serializer_class = CombinedSerializer
 
     def get_queryset(self):
-        return Collecte.objects.all()
-    
-    queryset = Collecte.objects.all()
+        return Csp.objects.all()
 
+class CombinedModelViewSet(viewsets.ModelViewSet):
+    serializer_class = CombinedSerializer
+    queryset = Collecte.objects.all()
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = {
+        'id_client__id_csp__csp_lbl': ['exact'],
+        'id_article__categorie_achat': ['exact'],
+        'date_collecte': ['icontains']
+    }
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        csp = self.request.query_params.get('csp')
+        category = self.request.query_params.get('cat_achat')
+        period = self.request.query_params.get('period')
+
+        print(f"csp: {csp}, cat_achat: {category}, period: {period}")  # Impression de débogage
+
+        if csp:
+            queryset = queryset.filter(id_client__id_csp__csp_lbl=csp)
+        if category:
+            queryset = queryset.filter(id_article__categorie_achat=category)
+        if period:
+            queryset = queryset.filter(date_collecte__icontains=period)
+
+        print(f"Queryset: {queryset.query}")  # Impression de débogage
+
+        return queryset
