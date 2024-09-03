5e9f4ee9990b9784e7f116616b4cf2db0c1af27e
Ydesaulty <yrae9@hotmail.fr>
2024-09-03 07:11:26 +0200

    Documentation_Backend_Swagger

--- Projet_Studi_GoldenLine/Projet_Studi_GoldenLine/urls.py
+++ Projet_Studi_GoldenLine/Projet_Studi_GoldenLine/urls.py
@@ -29,10 +29,24 @@ from indicateur.views import HomeView, LogoutView
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Collecte API",
+      title="API GoldenLine",
       default_version='v1',
-      description="API utilisee pour generer un indicateur de panier moyen",
-      contact=openapi.Contact(email="yrae9@hotmail.com"),      
+      description="API RESTful développée à partir des frameworks Django et Django REST Framework (DRF). "
+                  "Cette API est conçue pour gérer et exposer des ressources relatives au projet GoldenLine. "
+                  "Elle a pour objectif de renvoyer les données relatives aux collectes de manière anonymisée, "
+                  "et selon les critères définis via le frontend et au format JSON. \n\n"
+                  "L'API prend en charge l'authentification via JSON Web Tokens (JWT), garantissant une communication "
+                  "sécurisée et l'accès autorisé aux ressources. Les utilisateurs doivent s'authentifier pour pouvoir "
+                  "accéder aux données, tandis que des fonctionnalités de filtrage et de recherche sont disponibles "
+                  "pour faciliter l'accès aux données demandées.\n\n"
+                  "Les principales fonctionnalités de l'API incluent :\n"
+                  " • Gestion des utilisateurs : via l’interface d’administration, Création, mise à jour, et gestion des profils utilisateurs.\n"
+                  " • Authentification : Utilisation de JWT pour sécuriser l'accès aux endpoints.\n"
+                  " • Gestion des indicateurs : Création, mise à jour, suppression, et récupération des indicateurs de performance.\n"
+                  " • Filtrage et recherche : via l’interface d’administration, Recherche avancée et filtrage dynamique des données grâce à l'intégration de Django Filters.\n"
+                  " • Support CORS : Support des requêtes Cross-Origin pour une intégration front-end fluide.\n\n"
+                  "Cette API est destinée à être utilisée avec l’application front-end.",
+      contact=openapi.Contact(email="yrae9@hotmail.com"),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
