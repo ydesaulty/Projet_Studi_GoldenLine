31def745af437dbc83e223bfa34c59cff20c55f0
Ydesaulty <yrae9@hotmail.fr>
2024-09-08 14:01:48 +0200

    Modification_Deploiement

--- Projet_Studi_GoldenLine/Projet_Studi_GoldenLine/wsgi.py
+++ Projet_Studi_GoldenLine/Projet_Studi_GoldenLine/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projet_Studi_GoldenLine.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projet_Studi_GoldenLine.Projet_Studi_GoldenLine.settings')
 
 application = get_wsgi_application()
